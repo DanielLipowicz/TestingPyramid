2d60642a6e17b408482a83d135716cb69582e759
Daniel Lipowicz <lipowiczdaniel@gmail.com>
2023-10-08 21:39:35 +0200

    Modify test to provide equal number of test cases as on Unit level.

--- application/src/test/java/com/checkit/TestingPyramid/e2e/api/scenarios/AgeVerification.java
+++ application/src/test/java/com/checkit/TestingPyramid/e2e/api/scenarios/AgeVerification.java
@@ -1,22 +1,38 @@
 package com.checkit.TestingPyramid.e2e.api.scenarios;
 
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.containsString;
 
 public class AgeVerification {
 
-    @Test
-    public void testVerifyAgeEndpointWithAge15() {
+    static Stream<AgeTestData> ageTestData() {
+        return Stream.of(
+                new AgeTestData(-5, "are not eligible"),
+                new AgeTestData(0, "are not eligible"),
+                new AgeTestData(17, "are not eligible"),
+                new AgeTestData(18, "are eligible"),
+                new AgeTestData(25, "are eligible"),
+                new AgeTestData(99, "are eligible"),
+                new AgeTestData(199, "are eligible")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("ageTestData")
+    public void testVerifyAgeEndpointWithAge15(AgeTestData ageTestData) {
         RestAssured.baseURI = "http://localhost";
         RestAssured.port = 8080;
         RestAssured
                 .when()
-                .get("/api/verify-age?age=15")
+                .get("/api/verify-age?age=".concat(String.valueOf(ageTestData.age)))
                 .then().log().all()
                 .statusCode(200)
-                .body(containsString("are not eligible"));
+                .body(containsString(ageTestData.message));
     }
 
     private record AgeTestData(int age, String message) {
